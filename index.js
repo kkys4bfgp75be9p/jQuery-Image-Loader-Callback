6551a322c67523f9183f7d59c10933c1b315c2cb
James Charlesworth <james.charlesworth@gmail.com>
2011-12-11 21:00:54 -0500

    fixed spacing

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 (function( $ ) {
-	$.fn.loadImage = function(src,cb,image) {
-	    var self = this,
-			image = image || new Image();
-    
-		setTimeout(function() {
-	    	if (image.src != src)
-				image.src = src;
-	       if (!image.complete)
-	         return self.loadImage(src,cb,image)
+  $.fn.loadImage = function(src,cb,image) {
+    var self = this,
+    image = image || new Image();
 
-	      $rocketJ(self).attr('src',src)
-	      cb.call(this)
-	    },50);
-	};
+    setTimeout(function() {
+      if (image.src != src)
+        image.src = src;
+      if (!image.complete)
+        return self.loadImage(src,cb,image)
+
+      $rocketJ(self).attr('src',src)
+      cb.call(this)
+    },50);
+  };
 });
